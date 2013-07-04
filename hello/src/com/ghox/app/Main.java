193bc633996076a4aa8a5dd6147d55db1f980bae
camghox <camghox@gmail.com>
2013-07-04 23:21:47 +0800

    fix bug.

--- hello/src/com/ghox/app/Main.java
+++ hello/src/com/ghox/app/Main.java
@@ -6,7 +6,7 @@ public final class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("You say: Hello World!");
 	}
 
 }
